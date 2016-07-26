674d4c0e5ba1be7a18b6a5fd036e162fb40a990e
Martin Deshaies-Jacques <martin.deshaies-jacques@canada.ca>
2016-07-26 13:42:11 -0400

    Licence

--- DM93/gridCls.py
+++ DM93/gridCls.py
@@ -1,3 +1,23 @@
+#-------------------------- LICENCE BEGIN ---------------------------
+# This file is part of DaleyMenard93.
+#
+# DaleyMenard93 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# DaleyMenard93 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with DaleyMenard93.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors - Martin Deshaies-Jacques, Richard Menard
+#
+# Copyright 2016 - Air Quality Research Division, Environnement Canada
+#-------------------------- LICENCE END -----------------------------
 import numpy as np 
 
 class Grid(object):
